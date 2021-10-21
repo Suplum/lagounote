bd1fc8c3bbe917e81bd5792cfe5ed1a18be1a268
Suplum <Suplum@gmail.com>
2021-10-21 16:08:20 +0800

    闭包
    1. 模拟常用高阶函数：map、every、some
    2. 闭包概念
    3. 闭包案例

--- 函数/3.高阶函数的意义.js
+++ 函数/3.高阶函数的意义.js
@@ -1,3 +1,15 @@
 // 使用高阶函数的意义
 // 抽象可以帮我们屏蔽细节，只需要关注我们的目标
 // 高阶函数是用来抽象通用的问题
+
+// 面向过程的方式
+let array = [1, 2, 3, 4]
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i])
+}
+
+// 高阶函数
+// let array1 = [1, 2, 3, 4]
+// forEach(array1, item => {
+//   console.log(item)
+// })
