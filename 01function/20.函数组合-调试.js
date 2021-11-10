baf5f7311264662a93680b5c9230a11ecb5cc2e5
Suplum <Suplum@gmail.com>
2021-11-10 17:10:52 +0800

    函数组合-调试

---
+++ 01function/20.函数组合-调试.js
@@ -0,0 +1,29 @@
+// NEVER SAY BEY --> never-say-bye
+const _ = require('lodash')
+
+// _.split()
+const split = _.curry((sep, str) => _.split(str, sep))
+
+// _.toLower()
+
+
+// _.join()
+const join = _.curry((sep, array) => _.join(array, sep))
+
+// const log = v => {
+//   console.log(v)
+//   return v
+// }
+
+const trace = _.curry((tag, v) => {
+  console.log(tag, v)
+  return v
+})
+
+// _.map()
+const map = _.curry((fn, array) => _.map(array, fn))
+
+// const f = _.flowRight(join('-'), log, _.toLower, split(' '))
+const f = _.flowRight(join('-'), trace('map 之后'), map(_.toLower), trace('split 之后'), split(' '))
+
+console.log(f('NEVER SAY BEY'))
